cf59e74ebfb458802af1442c49c032d7905e3d81
Daniel Rowe <danielrowetech@gmail.com>
2020-02-12 13:51:33 -0800

    Implement listing ids

--- sdc_data_generator/index.js
+++ sdc_data_generator/index.js
@@ -1,30 +1,38 @@
+/* eslint-disable no-await-in-loop */
 /* eslint-disable no-console */
 const helpers = require('./helpers/file');
 const getRatings = require('./helpers/ratings');
 
 const { processFile, csvWriteStream } = helpers;
 
-const reviewsFile = 'reviews.csv';
-const seedFile = 'seedData.csv';
+const REVIEWS_FILE = 'reviews.csv';
+const SEED_FILE = 'seedData.csv';
+const LISTING_COUNT = 1000;
+let currentListingNum = 0;
+let currentReviewNum = 0;
+let currentListingReviewCount = Math.floor(Math.random() * 10) + 1;
+
 
 console.log('Generating and writing seed data to file...');
-let entryCount = 0;
 
 csvWriteStream.init(
-  seedFile,
+  SEED_FILE,
   [
-    'name', 'date', 'comment', 'overall_rating',
-    'accuracy_rating', 'location_rating',
-    'check_in_rating', 'value_rating',
-    'cleanliness_rating', 'communication_rating',
+    'listing_id', 'name', 'date', 'comment', 'overall_rating',
+    'accuracy_rating', 'location_rating', 'check_in_rating',
+    'value_rating', 'cleanliness_rating', 'communication_rating',
   ],
 );
 
 
 const appendData = (data) => {
-  const name = data.reviewer_name;
+  if (currentListingNum > LISTING_COUNT - 1) {
+    return;
+  }
+
+  const name = data.reviewer_name.trim().replace(/,/g, '');
   const { date } = data;
-  const comment = `"${data.comments.trim()}"`;
+  const comment = `"${data.comments.trim().replace(/"/g, '')}"`;
 
   const {
     overall,
@@ -36,19 +44,34 @@ const appendData = (data) => {
     communication,
   } = getRatings();
 
+  if (currentReviewNum >= currentListingReviewCount) {
+    currentListingReviewCount = Math.floor(Math.random() * 10) + 1;
+    currentListingNum += 1;
+    currentReviewNum = 0;
+  }
 
-  entryCount += 1;
 
   csvWriteStream.writeRow(
     [
-      name, date, comment, overall, accuracy, location,
-      checkIn, value, cleanliness, communication,
+      currentListingNum, name, date, comment, overall, accuracy,
+      location, checkIn, value, cleanliness, communication,
     ],
   );
+
+  currentReviewNum += 1;
 };
 
 
-processFile(reviewsFile, appendData).then(() => {
+const generate = async () => {
+  let pass = 1;
+
+  while (currentListingNum < LISTING_COUNT - 1) {
+    await processFile(REVIEWS_FILE, appendData);
+    console.log(`Completed pass ${pass} (${currentListingNum} listings written)`);
+    pass += 1;
+  }
+  console.log(`\nSuccess! \nGenerated ${currentListingNum} listings`);
   csvWriteStream.end();
-  console.log(`Successfully generated ${entryCount} entries!`);
-});
+};
+
+generate();
