8bb9affaa2e8a7ef8eaea55db19bfda6b0a5e417
Daniel Rowe <danielrowetech@gmail.com>
2020-02-18 10:11:03 -0800

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,34 @@
 
+const path = require('path');
+
 module.exports = {
-  entry: __dirname + "/client/src/index.jsx",
+  entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [ "babel-loader"],
-      }, 
+        use: ['babel-loader'],
+      },
       {
         test: /\.css$/,
-        use: [{loader: 'style-loader'}, 
-        {loader: 'css-loader',
-        options: {
-          modules: true,
-          importLoaders: 1,
-          localIdentName: '[sha1:hash:hex:6]',
-        },}],
-      }
-    ]
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[sha1:hash:hex:6]',
+            },
+          },
+        ],
+      },
+    ],
   },
+  resolve: { extensions: ['.jsx', '.js'] },
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/client/dist/'
-  }
+    path: path.resolve(__dirname, 'client', 'dist'),
+  },
 };
