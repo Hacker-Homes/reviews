232bc176e0313bad56b3d9373086ec9f70eddf1d
Ubuntu <ubuntu@ip-172-31-31-248.us-west-2.compute.internal>
2020-02-26 01:03:35 +0000

    Increase max pool to 65

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const pool = new Pool({
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   database: 'homes',
-  max: 50, 
+  max: 65, 
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 500,
 });
